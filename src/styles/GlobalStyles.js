b46b0a4018b7aec2906374118db53f6e2b3ef0ff
Mohammed Sahal <118444006+mohammmmmmmmmmed@users.noreply.github.com>
2024-04-16 03:09:45 +0530

    Add files via upload

--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -2,32 +2,36 @@ import { createGlobalStyle } from "styled-components";
 import "@fontsource/akaya-telivigala";
 import "@fontsource/sora";
 
-const GlobalStyles = createGlobalStyle`
+// font-family: "Akaya Telivigala", cursive;
+// font-family: "Sora", sans-serif;
 
-/* *{
-    outline: 1px solid red !important; 
-} */
+export const GlobalStyles = createGlobalStyle`
+  *,*::before,*::after{
+    margin:0;
+    padding:0;
+  }
 
+  html{
+    overflow-x:hidden;
+  }
 
-*,*::before,*::after{
-    margin: 0;
-    padding: 0;
-}
+  .animate__delay{
+    animation-delay:.5s;
+  }
 
-body{
-    font-family: 'Sora', sans-serif;
-    overflow-x: hidden;
-}
+  body{
+    font-family: "Sora", sans-serif;
+    overflow-x:hidden;
+  }
 
-h1,h2,h3,h4,h5,h6{
-    margin: 0;
-    padding: 0;
-}
+  h1, h2 , h3 , h4  , h5 ,h6{
+     margin:0;
+     padding:0;
+  }
 
-a{
-    color: inherit;
+  a{
+    color:inherit;
     text-decoration: none;
-}
-`
-
-export default GlobalStyles;
+    
+  }
+`;
